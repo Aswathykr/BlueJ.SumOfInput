32e7e5191d94ea14b96a800187cff31ae10d735f
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-5.local>
2019-02-06 17:44:31 -0500

    implemented Sum of Input and tested

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
+import java.util.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        try{
+            int number = scanner.nextInt();
+            for(int i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+            System.out.printf("The sum of numbers upto %d is %d\n", number, sum);
+        }
+        catch(Exception e)
+        {
+            System.out.printf("This works only on numbers!!");
+        }
     }
 }
